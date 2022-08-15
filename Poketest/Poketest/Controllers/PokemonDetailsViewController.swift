f4380d582c55a44594814e01adfacba2146bfc17
Marcelo Macedo <marcelomacedodev@gmail.com>
2022-08-15 23:17:33 +0100

    [NEW] Update details layout to have all the info needed. [TODO] Add constraints & Start logic for details

--- Poketest/Poketest/Controllers/PokemonDetailsViewController.swift
+++ Poketest/Poketest/Controllers/PokemonDetailsViewController.swift
@@ -11,7 +11,14 @@ import SDWebImage
 class PokemonDetailsViewController: UIViewController {
     
     @IBOutlet weak var pokemonImage: UIImageView!
-    @IBOutlet weak var pokemonNameLabel: UILabel!
+    @IBOutlet weak var pokemonDescriptionLabel: UILabel!
+    @IBOutlet weak var pokemonXPLabel: UILabel!
+    @IBOutlet weak var pokemonHPLabel: UILabel!
+    @IBOutlet weak var pokemonType1Label: UILabel!
+    @IBOutlet weak var pokemonType2Label: UILabel!
+    @IBOutlet weak var pokemonHeightLabel: UILabel!
+    @IBOutlet weak var pokemonWeightLabel: UILabel!
+    
     
     var pokemon: Pokemon?
     
@@ -38,9 +45,14 @@ class PokemonDetailsViewController: UIViewController {
         } else if let defaultURL = pokemon?.sprites?.frontDefault{
             pokemonImage.sd_setImage(with: URL(string: defaultURL), placeholderImage: UIImage(named: "placeholder.png"))
         }
-        pokemonNameLabel.text = pokemon?.name?.capitalizingFirstLetter()
+        pokemonDescriptionLabel.text = pokemon?.name?.capitalizingFirstLetter()
     }
     
 }
 
 
+/* --- Notes ---
+ weight -> hectograms
+ height -> decimetres
+ 
+ */
